9d0e7fba4ed2b8fb54e718cd9909e83cb8c4e656
Clayton Ogutu <claytonleroy9@gmail.com>
2026-05-02 21:03:52 +0300

    Change email configuration to example credentials
    Updated email configuration to use example credentials.

--- app.py
+++ app.py
@@ -26,9 +26,9 @@ mysql = MySQL(app)
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'claytonogutu@gmail.com'
-app.config['MAIL_PASSWORD'] = 'pbgi wlhp donb kxve'
-app.config['MAIL_DEFAULT_SENDER'] = 'claytonogutu@gmail.com'
+app.config['MAIL_USERNAME'] = 'example@gmail.com'
+app.config['MAIL_PASSWORD'] = '**** **** **** ****'
+app.config['MAIL_DEFAULT_SENDER'] = 'example@gmail.com'
 mail = Mail(app)
 
 # ─── EMAIL HELPER ─────────────────────────────────────────
